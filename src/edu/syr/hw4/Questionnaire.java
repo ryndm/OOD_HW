bc570442c578e233ba54d3b2665ae179c59a9390
Ryan <dmelloryan11@gmail.com>
2023-09-24 19:44:02 -0400

    HW04

--- src/edu/syr/hw4/Questionnaire.java
+++ src/edu/syr/hw4/Questionnaire.java
@@ -1,2 +1,80 @@
-package edu.syr.hw4;public class Questionnaire {
+// rdmello
+package edu.syr.hw4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Questionnaire {
+    List<String> questions;
+    public Questionnaire() {
+        questions = new ArrayList<>();
+    }
+    public void addQuestion(String s) {
+        questions.add(s);
+    }
+
+    private String getValidResponse(BufferedReader reader, String question) {
+        while (true) {
+            System.out.println(question);
+
+            if (question.contains("Leikert")) {
+                System.out.println("Please respond with an integer from 1 to 5:");
+                System.out.println("1 - Strongly Agree");
+                System.out.println("2 - Somewhat Agree");
+                System.out.println("3 - Neutral");
+                System.out.println("4 - Somewhat Disagree");
+                System.out.println("5 - Strongly Disagree");
+            } else {
+                System.out.println("True or False: ");
+            }
+
+            try {
+                String response = reader.readLine();
+
+                if (isValidResponse(response, question)) {
+                    return response;
+                } else {
+                    System.out.println("Invalid response. Please enter 'True', 'False', or a valid integer for Leikert scale.");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean isValidResponse(String response, String question) {
+        if (question.contains("Leikert")) {
+            try {
+                int likertResponse = Integer.parseInt(response);
+                return likertResponse >= 1 && likertResponse <= 5;
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        } else {
+            return response.equalsIgnoreCase("true") || response.equalsIgnoreCase("false");
+        }
+    }
+    public List<String> administerQuestionnaire() {
+        List<String> answers = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (String s: questions) {
+            String response = getValidResponse(reader, s);
+            answers.add(response);
+        }
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Questionnaire q = new Questionnaire();
+        q.addQuestion("Are you awake?");
+        q.addQuestion("Have you had coffee?");
+        q.addQuestion("Are you ready to get to work?");
+        q.addQuestion("CSE 687 is awesome (Leikert):");
+        List<String> answers = q.administerQuestionnaire();
+        System.out.println("complete!");
+        System.out.println(answers);
+    }
 }
--- src/edu/syr/hw4/Rational.java
+++ src/edu/syr/hw4/Rational.java
@@ -1,2 +1,77 @@
-package edu.syr.hw4;public class Rational {
+// rdmello
+package edu.syr.hw4;
+
+/* Adapted from Programming in Scala, Odersky */
+
+public class Rational {
+    private int numer;
+    private int denom;
+    private int g;
+    public Rational(int n, int d) { /*implement this*/
+        this.g = this.calculateGCD(n, d);
+        this.numer = n/this.g;
+        this.denom = d/this.g;
+    }
+
+    public Rational(int n) {/*implement this*/
+        this(n, 1);
+    }
+
+    private int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+
+    @Override
+    public String toString() {/*implement this*/
+        return this.numer + "/" + this.denom;
+    }
+
+    public Rational add(Rational that) {/*implement this*/
+        int newNumer = this.numer * that.denom + that.numer * this.denom;
+        int newDenom = this.denom * that.denom;
+        int newGcd = calculateGCD(newNumer, newDenom);
+        return new Rational(newNumer / newGcd, newDenom / newGcd);
+    }
+
+    public Rational add(int that) {/*implement this*/
+        return this.add(new Rational(that));
+    }
+
+    public boolean lessThan(Rational that) {/*implement this*/
+        return this.numer * that.denom < that.numer * this.denom;
+    }
+
+    public boolean lessThan(int that) {/*implement this*/
+        return this.lessThan(new Rational(that));
+    }
+
+    public Rational max(Rational that) {/*implement this*/
+        return this.lessThan(that) ? that : this;
+    }
+
+    public Rational max(int that) {/*implement this*/
+        return this.max(new Rational(that));
+    }
+
+    public static void main(String[] args) {
+        Rational half = new Rational(1, 2);
+        Rational third = new Rational(1, 3);
+        System.out.println(half.max(third)); // returns half; prints "1/2"
+        System.out.println(third.max(half)); // returns half; prints "1/2"
+        Rational result = half.add(third);
+        Rational fiveSixths = new Rational(5, 6);
+        System.out.println("are they the same object? " + (result == fiveSixths));
+        System.out.println(fiveSixths.lessThan(result)); // false
+        System.out.println(result.lessThan(fiveSixths)); // false
+        Rational one = new Rational(1);
+        Rational oneAndHalf = one.add(half);
+        System.out.println(oneAndHalf); // prints "3/2";
+        Rational fifteenOverTen = new Rational(15, 10);
+        System.out.println(fifteenOverTen); // prints "3/2"
+    }
 }
